621ab29e19e0a1b5ec21a6aa49c594912fd845bd
aarnaesthetic <96248343+aarnaesthetic@users.noreply.github.com>
2022-02-09 20:41:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristx=0;
+rightWristy=0;
 
 function preload()
 {
@@ -15,9 +19,40 @@ function setup()
 
     video= createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+     console.log(results);
+     leftWristX= results[0].pose.leftWrist.x;
+     leftWristY= results[0].pose.leftWrist.y;
+     console.log("Left Wrist x= "+ leftWristX+" Left Wrist Y= "+leftWristY)
+     rightWristx= results[0].pose.rightWrist.x;
+     rightWristy=results[0].pose.rightWrist.y;
+     console.log("Right Wrist X= "+rightWristx+" Right Wrist Y= "+rightWristy);
+
+    }
+}
+
 
 function draw()
 {
     image(video,0,0,600,500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1)
 }
